9d7e6a81a11a6d551916ea815c2427b17b9693b3
jrt-20 <2046930970@qq.com>
2021-11-17 20:06:08 +0800

    logback优化日志

---
+++ work/src/main/java/com/nosql/work/Controller/IndexController.java
@@ -0,0 +1,14 @@
+package com.nosql.work.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping("/index")
+    public String  index(){
+        return "news";
+    }
+}
--- work/src/main/java/com/nosql/work/WorkApplication.java
+++ work/src/main/java/com/nosql/work/WorkApplication.java
@@ -1,10 +1,13 @@
 package com.nosql.work;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class WorkApplication {
+
 
     public static void main(String[] args) {
         SpringApplication.run(WorkApplication.class, args);
